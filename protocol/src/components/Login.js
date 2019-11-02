5189f37b1bf2b2aed97dbfe0b5c04c748f7e0ddc
Miguel <ginji-amano@hotmail.com>
2019-11-02 11:07:50 -0500

    Agregar - Eliminar
    Falta seleccionar
    Ver la pantalla de Unity

--- protocol/src/components/Login.js
+++ protocol/src/components/Login.js
@@ -1,27 +1,72 @@
 import React, {Component} from 'react';
 import '../App.css';
+import {users} from '../users.json';
+import UserForm from './UserForm';
 
 class Login extends Component {
-    handleInput() {
-        console.log('writing...');
+    constructor() {
+        super();
+        this.state = {
+            users
+        };
+        this.handleAddUser = this.handleAddUser.bind(this);
     }
+
+    handleAddUser(user) {
+        this.setState({
+            users: [...this.state.users, user]
+        })
+    }
+
+    handleRemoveUser(index) {
+        if (window.confirm('¿Seguro de querer eliminar?')) {
+            this.setState({
+                users: this.state.users.filter((e,i) => {
+                    return i !== index
+                })
+            })
+        }
+    }
+
     render() {
-        return (
-            <div className = "card">
-                <form className="card-body">
-                    <div className="form-group">
-                        <input
-                        type = "text"
-                        name = "nombre"
-                        onChange={this.handleInput}
-                        className = "form-control"
-                        placeholder = "Nombre"
-                        />
-                     
+        const users = this.state.users.map((user,i) => {
+            return(
+                <div className = "col-md-4" key={i}>    
+                    <div className="card mt-4">
+                        <div className="card-header">
+                            <h3>{user.id}</h3>
+                            <span className= "badge badge-pill badge-danger ml-2">
+                            Estado: {user.state}
+                            </span>
+                        </div>
+                        <div className = "card-body">
+                            <p>Protocolo de entrenamiento: {user.protocol}</p>
+                        </div>
+                        <div className = "card-footer">
+                            <button className = "btn btn-danger" onClick={this.handleRemoveUser.bind(this,i)}>
+                                Eliminar
+                            </button>
+                        </div>
                     </div>
-                </form>
+                </div>
+            )
+        })
+        return (
+            <div className = "App">
+                <div className = "container">
+                    <div className = "row mt-4">
+                        <div className = "col-md-3">
+                            <UserForm onAddUser= {this.handleAddUser}/>
+                        </div>
+                        <div className = "col-md-9">
+                            <div className = "row">
+                            {users}
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-        )
+        );
      }
 }
 

084cfac7367ca7208135b969732d68607fc37db9
Miguel <ginji-amano@hotmail.com>
2019-11-09 11:55:44 -0500

    Ver detalle
    Falta poder seleccionar solo un usuario / hacer más visible la tabla

--- protocol/src/components/Detail.js
+++ protocol/src/components/Detail.js
@@ -1,12 +1,41 @@
 import React, {Component} from 'react';
 import '../App.css';
 import {Link} from 'react-router-dom';
+import {users} from '../users.json';
 
 class Detail extends Component {
+    constructor() {
+        super();
+        this.state = {
+            users
+        };
+    }
     render() {
+        const users = this.state.users.map((user,i) => {
+            return(
+                <table className = "blueTable">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">id</th>
+                            <th scope="col">state</th>
+                            <th scope="col">protocol</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">{i}</th>
+                            <td>{user.id}</td>
+                            <td>{user.state}</td>
+                            <td>{user.protocol}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            )
+        })
         return (
             <div>
-                <h1>Aca irá el detalle del perfil</h1>
+                {users}
                     <Link to="/home">
                         <button className = "btn btn-primary">
                             Regresar
